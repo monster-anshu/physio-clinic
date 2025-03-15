1ec7ffe6476e927d6f33909418d620e2656d25e5
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-15 05:57:35 +0000

    feat: install payload cms

---
+++ src/payload.config.ts
@@ -0,0 +1,24 @@
+import { postgresAdapter } from "@payloadcms/db-postgres";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { buildConfig } from "payload";
+import sharp from "sharp";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+  editor: lexicalEditor(),
+  collections: [],
+  secret: process.env.PAYLOAD_SECRET || "",
+  typescript: {
+    outputFile: path.resolve(dirname, "payload-types.ts"),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI || "",
+    },
+  }),
+  sharp,
+});
